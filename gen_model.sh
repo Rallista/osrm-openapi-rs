7fba6bec96f11d981e945757edb5244999306dff
Jacob Fielding <jf0517@gmail.com>
2025-12-14 12:06:48 -0800

    fix: banner content maneuver type and modifier

--- gen_model.sh
+++ gen_model.sh
@@ -1,7 +1,7 @@
 openapi-generator generate \
   -i openapi.yaml \
   -g rust \
-  --additional-properties=packageName=osrm \
+  --additional-properties=packageName=osrm-openapi-models \
   -o ./
 
   # --global-property models \
